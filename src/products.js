8a31e7a2e3c112b4c20d6039e51c355ab1c83218
Illia Kazmin <kazmin1c@gmail.com>
2022-06-26 23:50:41 +0200

    complete section products
    Removed focus from buttons
    Fix the position of pictures

--- src/products.js
+++ src/products.js
@@ -11,15 +11,18 @@
         moreBtnRef.classList.toggle("is-open");
 
         moreRef.classList.toggle("is-open");
+        moreBtnRef.blur();
     });
     moreBtnRef1.addEventListener("click", () => {
         moreBtnRef1.classList.toggle("is-open");
 
         moreRef1.classList.toggle("is-open");
+        moreBtnRef1.blur();
     });
     moreBtnRef2.addEventListener("click", () => {
         moreBtnRef2.classList.toggle("is-open");
 
         moreRef2.classList.toggle("is-open");
+        moreBtnRef2.blur();
     });
 })();
