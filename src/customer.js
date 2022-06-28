d3ef1396cc22497e5a37d1c227e4e35be3c0b9f1
IrynaMala <malaschinska@gmail.com>
2022-06-28 08:11:12 +0300

    Adds almost all tasks

---
+++ src/customer.js
@@ -0,0 +1,13 @@
+// <!-- Swiper JS -->
+//     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+//     <!-- Initialize Swiper -->
+//     <script>
+//       var swiper = new Swiper(".mySwiper", {
+//         spaceBetween: 30,
+//         pagination: {
+//           el: ".swiper-pagination",
+//           clickable: true,
+//         },
+//       });
+//     </script>
