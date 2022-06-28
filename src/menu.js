ecbfd806efdd96443c3ca11fea9923a4fed34101
Dmytro Tarasenko <dm.tarasenko97@gmail.com>
2022-06-28 19:47:28 +0300

    Додав модалку хедера

--- src/menu.js
+++ src/menu.js
@@ -4,8 +4,6 @@
   const closeMenuBtn = document.querySelector('.menu__btn--close');
   const menuItemSelect = document.querySelector('.menu__list');
 
-  
-
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -22,9 +20,8 @@
   closeMenuBtn.addEventListener('click', toggleMenu);
 
   menuItemSelect.addEventListener('click', () => {
-   mobileMenu.classList.remove('is-open');
-  })
-  
+    mobileMenu.classList.remove('is-open');
+  });
 
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
@@ -32,4 +29,30 @@
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('is-open');
+  }
+})();
+
+(() => {
+  const refs1 = {
+    openModalBtn1: document.querySelector('[data-modal-open1]'),
+    modal1: document.querySelector('[data-modal1]'),
+  };
+  refs1.openModalBtn1.addEventListener('click', toggleModal1);
+  function toggleModal1() {
+    refs1.modal1.classList.toggle('is-hidden');
+    document.body.classList.toggle('is-open');
+  }
 })();
