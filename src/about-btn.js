b5bf4f8a3e2748710d306b504348a81fd8d59ea9
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-30 11:26:02 +0300

    remove my change

--- src/about-btn.js
+++ src/about-btn.js
@@ -6,9 +6,9 @@ const aboutSubDesc = document.querySelector('.about__subdescription--more');
 readMoreBtn.addEventListener('click', (evt) => {
    aboutSubDesc.classList.toggle('show-more');
     readMoreBtn.classList.toggle('clicked');
-    if (!readMoreBtn.classList.value.split(' ').includes('clicked')) {
-      readMoreBtn.blur();
-    }
+    // if (!readMoreBtn.classList.value.split(' ').includes('clicked')) {
+    //   readMoreBtn.blur();
+    // }
 })
 
 })();
