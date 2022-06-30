c7764147cc095b02d4b03b48292e3d223c47873d
ikarkam76 <104020905+ikarkam76@users.noreply.github.com>
2022-06-30 07:40:32 +0000

    Deploying to gh-pages from @ ikarkam76/fson55-html-css-project-10@d8ee3dc0838b6c9d8844a2863047717ead8892c4 🚀

--- index.636254f2.js
+++
@@ -1,2 +0,0 @@
-var btns=document.querySelectorAll(".contacts__button"),modalOverlay=document.querySelector(".modal-overlay "),modals=document.querySelectorAll(".button__options");btns.forEach((function(t){t.addEventListener("click",(function(t){var e=t.currentTarget.getAttribute("data-path");modals.forEach((function(t){t.classList.remove("modal--visible")})),document.querySelector('[data-target="'.concat(e,'"]')).classList.add("modal--visible"),modalOverlay.classList.add("modal-overlay--visible")}))}));
-//# sourceMappingURL=index.636254f2.js.map
